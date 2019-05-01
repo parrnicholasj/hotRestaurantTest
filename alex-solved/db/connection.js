cea53fffc299fa1550a0bb2f581912db786028c8
Nicholas Parr <parrnicholasj@gmail.com>
2019-05-01 14:07:06 -0400

    testing heroku

--- alex-solved/db/connection.js
+++ alex-solved/db/connection.js
@@ -8,7 +8,6 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "300Atrium",
     database: "restaurant_db"
